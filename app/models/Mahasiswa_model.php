6129082af2eca1a0f2d64dbcb19f0cd4aa1f5150
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-06-27 20:09:46 +0700

    Fetch data from database in model

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,22 +1,24 @@
 <?php
 
 class Mahasiswa_model {
-  private $mhs = [
-    [
-      'nama' => 'Rahman Fadhil',
-      'nrp' => '298347189234',
-      'email' => 'rhmnfadhil@gmail.com',
-      'jurusan' => 'Teknik Informatika'
-    ],
-    [
-      'nama' => 'Rahman Fadhil',
-      'nrp' => '298347189234',
-      'email' => 'rhmnfadhil@gmail.com',
-      'jurusan' => 'Teknik Informatika'
-    ]
-  ];
+  private $dbh;
+  private $stmt;
+
+  public function __construct() {
+    // data source name
+    $dsn = 'mysql:host=localhost;dbname=phpmvc;';
+
+    try {
+      $this->dbh = new PDO($dsn, 'root', '12345');
+    } catch (PDOException $e) {
+      die($e->getMessage());
+    }
+  }
 
   public function getAllMahasiswa() {
-    return $this->mhs;
+    $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
+    $this->stmt->execute();
+
+    return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
   }
 }
