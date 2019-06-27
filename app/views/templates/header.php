69663a42a54b712071d2808aab2224fcc99510e3
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-06-27 17:46:08 +0700

    Add bootstrap

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -5,5 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title><?= $data['title'] ?></title>
+
+  <link rel="stylesheet" href="http://localhost/learn_mvc/public/css/bootstrap.min.css">
 </head>
 <body>
