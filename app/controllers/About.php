3ed5b63f4b4c060165d8578003c4d727864a930b
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-06-27 17:38:43 +0700

    Use templates

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -4,10 +4,14 @@ class About extends Controller {
   public function index($name = 'Fadhil', $age = 15) {
     $data['name'] = $name;
     $data['age'] = $age;
+    $this->view('templates/header');
     $this->view('about/index', $data);
+    $this->view('templates/footer');
   }
 
   public function page() {
+    $this->view('templates/header');
     $this->view('about/page');
+    $this->view('templates/footer');
   }
 }
--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -2,6 +2,8 @@
 
 class Home extends Controller {
   public function index() {
+    $this->view('templates/header');
     $this->view('home/index');
+    $this->view('templates/footer');
   }
 }
