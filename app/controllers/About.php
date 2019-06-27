fe93ee8b5e6a1ae56649f33fd2a6ed341eda59ce
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-06-27 17:40:17 +0700

    Use dynamic title

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -4,13 +4,13 @@ class About extends Controller {
   public function index($name = 'Fadhil', $age = 15) {
     $data['name'] = $name;
     $data['age'] = $age;
-    $this->view('templates/header');
+    $this->view('templates/header', ['title' => 'About']);
     $this->view('about/index', $data);
     $this->view('templates/footer');
   }
 
   public function page() {
-    $this->view('templates/header');
+    $this->view('templates/header', ['title' => 'Pages']);
     $this->view('about/page');
     $this->view('templates/footer');
   }
--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -2,7 +2,7 @@
 
 class Home extends Controller {
   public function index() {
-    $this->view('templates/header');
+    $this->view('templates/header', ['title' => 'Home']);
     $this->view('home/index');
     $this->view('templates/footer');
   }
